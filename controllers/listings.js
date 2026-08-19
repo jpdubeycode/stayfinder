bab240dd5ecc58858fd07a9cba04f04ff8ae8aaf
jpdubeycode <jaidubeyiitm@gmail.com>
2026-08-19 18:18:33 +0530

    AddProject Files

---
+++ controllers/listings.js
@@ -0,0 +1,89 @@
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
+const Listing = require("../models/listing.js");
+
+module.exports.index = async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+};
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("listings/new.ejs");
+};
+
+module.exports.showListing = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+        .populate({
+            path: "reviews",
+            populate: { path: "author" },
+        })
+        .populate("owner");
+
+    if (!listing) {
+        req.flash("error", "Listing you requested does not exist!");
+        return res.redirect("/listings");
+    }
+
+    res.render("listings/show.ejs", { listing });
+};
+
+module.exports.createListing = async (req, res) => {
+    let response = await geocodingClient
+        .forwardGeocode({
+            query: req.body.listing.location,
+            limit: 1,
+        })
+        .send();
+
+    if (!response.body.features.length) {
+        req.flash("error", "Location not found. Please enter a valid location.");
+        return res.redirect("/listings/new");
+    }
+
+    const newListing = new Listing(req.body.listing);
+
+    newListing.owner = req.user._id;
+
+    let url = req.file.path;
+    let filename = req.file.filename;
+    newListing.image = { url, filename };
+
+    newListing.geometry = response.body.features[0].geometry;   // ye line add karo
+
+    await newListing.save();
+    req.flash("success", "New Listing created!");
+    res.redirect("/listings");
+};
+
+   module.exports.renderEditForm = async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing})
+
+}
+
+module.exports.updateListing = async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    if (req.file) {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
+
+    req.flash("success", "Listing Updated!");
+    res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!");
+    res.redirect("/listings");
+};
+
---
+++ init/index.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const dbUrl = process.env.ATLASDB_URL;
+
+main()
+.then(() => {
+    console.log("connected to DB");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "652d0081ae547c5d37e56b5f" }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
+
+initDB();
