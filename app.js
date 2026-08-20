8052a12bd69deb7f23529c4de640a09dd54b08c6
jpdubeycode <jaidubeyiitm@gmail.com>
2026-08-20 09:58:50 +0530

    Prepare for deployment

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render("error.ejs",{message})
 });
 
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`server is listening to port ${port}`);
 });
